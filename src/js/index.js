7fe14b1c11b435016dd48bf9d85e2ea622bc9ad0
Kamila Zając <kamilaagata.zajac@gmail.com>
2021-04-29 14:58:07 +0200

    Hydrapp v 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,39 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { registerSW } from "./pwa.js";
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glassCounter = document.querySelector(".glass__counter--js");
+const buttonAdd = document.querySelector(".button-add--js");
+const buttonRemove = document.querySelector(".button-remove--js");
+
+const key = new Date().toLocaleString().slice(0, 10);
+
+let currentGlasses = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  currentGlasses = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+
+glassCounter.innerHTML = currentGlasses;
+
+buttonAdd.addEventListener("click", () => {
+  currentGlasses++;
+  glassCounter.innerHTML = currentGlasses;
+  localStorage.setItem(key, currentGlasses);
+});
+
+buttonRemove.addEventListener("click", () => {
+  if (currentGlasses > 0) {
+    currentGlasses--;
+  }
+  glassCounter.innerHTML = currentGlasses;
+  localStorage.setItem(key, currentGlasses);
+});
